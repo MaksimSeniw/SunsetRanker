70999528ad1dc5ea9d74f4cbee09bc8f21237086
MaksimSeniw <mase5436@colorado.edu>
2025-07-22 19:12:24 -0600

    test 3

--- Backend/config/gcs.js
+++ Backend/config/gcs.js
@@ -1,16 +1,13 @@
 const { Storage } = require('@google-cloud/storage');
 
-const serviceAccount = JSON.parse(process.env.GCS_KEY_JSON);
-
-const rawKey = JSON.parse(process.env.GCS_KEY_JSON);
-
-rawKey.private_key = rawKey.private_key.replace(/\\n/g, '\n');
+const decodedKey = Buffer.from(process.env.GCS_KEY_B64, 'base64').toString('utf-8');
+const credentials = JSON.parse(decodedKey);
 
 const storage = new Storage({
-  projectId: rawKey.project_id,
-  credentials: rawKey,
+  projectId: credentials.project_id,
+  credentials,
 });
 
-const bucket = storage.bucket('sunset-photo-uploads');
+const bucket = storage.bucket(process.env.GCS_BUCKET_NAME);
 
 module.exports = bucket;
